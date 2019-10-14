17ca44a8bd08b1c2518d4db3f8c1cc9ee9f9b896
zhangyu <2961242927@qq.com>
2019-10-14 12:01:41 +0800

    数据填充完成

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
 
     <div class="thunder">
-      <div v-for="(item, indexO) in 10"
+      <div v-for="(item, indexO) in defThunderArr"
            :key="indexO"
            class="flexbox">
 
@@ -11,7 +11,7 @@
              :data-id='returnId(indexO,index)'
              v-for="(item, index) in 10"
              :key="index">
-          {{returnId(indexO,index)}}
+          {{defThunderArr[indexO][index]}}
         </div>
       </div>
     </div>
@@ -24,6 +24,7 @@ export default {
       thunderTotalNum: 0,
       thunerArr: [],
       num2: 0,
+      defThunderArr: []
     }
   },
   watch: {
@@ -85,17 +86,19 @@ export default {
       // 查找周围雷的个数
       for (let tr = 0; tr < row; tr++) {
         for (let tc = 0; tc < col; tc++) {
-
+          // 如果当前项是雷
           if (defThunder[tr][tc] === -1) {
             console.log(defThunder[tr][tc]);
             // 如果当前是雷 则周围9个框数字加1
             // 上一行数字操作
             let id = 3
-            // 下标 从右到左 因为一开始就执行自减 所以要多加1
-            // 下标规则 如果是列的最后一位 就只需要加一即可 对于雷是列的最后一个进行的判断
-            let thSubScript = tc === 9 ? tc + 1 : tc + 2
+            // 雷上一行的处理逻辑
             if (tr > 0) {
+              // 下标 从右到左 因为一开始就执行自减 所以要多加1
+              // 下标规则 如果是列的最后一位 就只需要加一即可 对于雷是列的最后一个进行的判断
+              let thSubScript = tc === (col - 1) ? tc + 1 : tc + 2
               // debugger
+              if (tc === (col - 1)) id = 2
               for (let ti = 0; ti < id; ti++) {
                 // 上一行 并且不是第一行
                 // 如果当前项为地雷 则不进行操作 注意 -- 运算符是先赋值再减减
@@ -108,14 +111,42 @@ export default {
                     defThunder[tr - 1][sty]++
                   }
                 }
-                // 处理
               }
+            }
+            // 雷下一行的处理逻辑
+            // 声明下一行的下标 如果行的最后一个是雷 则下一行的下标与雷的下标相同 (默认会减一次 所以多加1)
+            let footerCel = col - 1
+            let underSubScriper = (tc === footerCel) ? tc + 1 : tc + 2
+            // 如果当前雷的位置是行的第一个或者最后一个就只需要循环两次（操作两次数据）即可
+            let colid = col - 1
+            let underSubFor = tc === 0 ? 2 : 3
+            underSubFor = tc === colid ? 2 : underSubFor
+            // 如果是最后一行就不进行操作
+            let forRow = row - 1
+            if (tr === forRow) return
+            for (let i = 0; i < underSubFor; i++) {
+              underSubScriper--
+              let underId = underSubScriper
+              // 如果是雷就不进行操作
+              if (defThunder[tr + 1][underId] !== -1) {
+                defThunder[tr + 1][underId]++
+              }
+            }
+            // -----------------------
+            // 左右 
+            if (tc === 0) {
+              defThunder[tr][tc + 1]++
+            } else if (tc === (col.length - 1)) {
+              defThunder[tr][tc - 1]++
+            } else {
+              defThunder[tr][tc - 1]++
+              defThunder[tr][tc + 1]++
             }
           }
         }
       }
       console.log(defThunder);
-
+      this.defThunderArr = defThunder
     },
     // 生成炸弹
     createThunder () {
@@ -204,6 +235,7 @@ body,
   .innerbox {
     width: 50px;
     height: 50px;
+    line-height: 50px;
     background-color: #ccc;
     border: 1px solid #007acc;
     box-sizing: border-box;
